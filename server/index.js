1ca7a8b37b257ccd9ca513f95288ed8bca8c4c2e
chetbaby <chet.hay@gmail.com>
2019-08-20 16:14:44 -0700

    added react-json-view. sample api endpoint

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// sample API
+app.get('/api', (req, res, next) => {
+  axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => res.send(response.data));
+});
 
 app.listen(8000, () => console.log(`Listening on port 8000!`));
--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ReactJson from 'react-json-view';
+import Axios from 'axios';
 
 function App() {
-  return <h1>hello world</h1>;
+  const [list, setList] = useState('');
+  useEffect(() => {
+    Axios.get('/api').then(result => setList(result.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>hello world</h1>
+      <ReactJson src={list} theme="hop scotch" />
+    </div>
+  );
 }
 
 export default App;
